6ed599c2707117039550a1e3b7c3970a6026ee52
Maciej Zagrajek <maciej.zagrajek@gmail.com>
2020-04-20 07:39:50 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KoloryWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            rectangle.Fill = new SolidColorBrush(Colors.Black);
+        }
+
+        private void sliderR_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Color color = Color.FromRgb(
+                (byte)sliderR.Value,
+                (byte)sliderG.Value,
+                (byte)sliderB.Value);
+            KolorProstokata = color;
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape) Close();
+        }
+
+        private Color KolorProstokata
+        {
+            get
+            {
+                return (rectangle.Fill as SolidColorBrush).Color;
+            }
+            set
+            {
+                (rectangle.Fill as SolidColorBrush).Color = value;
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Ustawienia.Zapisz(KolorProstokata);
+        }
+    }
+}
